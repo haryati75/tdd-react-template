dbb52b5b6817eb9054937039d70966196d27dc93
Haryati H <haryati75@gmail.com>
2025-12-21 11:21:00 +0800

    fix: allow vitest config property with correct defineConfig import

--- vite.config.ts
+++ vite.config.ts
@@ -1,20 +1,20 @@
 /// <reference types="vitest" />
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vitest/config";
+import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/tdd-react-template/',  // Adjust base path for GitHub Pages 
+  base: "/tdd-react-template/", // Adjust base path for GitHub Pages
   test: {
     globals: true,
-    environment: 'jsdom',
-    setupFiles: './src/setupTests.ts',
-    include: [ 'src/**/*.test.{ts,tsx}' ],
+    environment: "jsdom",
+    setupFiles: "./src/setupTests.ts",
+    include: ["src/**/*.test.{ts,tsx}"],
     coverage: {
-      provider: 'istanbul',
-      include: [ 'src/**/*.{ts,tsx}' ],
-      exclude: [ 'src/main.tsx' ],
+      provider: "istanbul",
+      include: ["src/**/*.{ts,tsx}"],
+      exclude: ["src/main.tsx"],
     },
   },
-})
+});
